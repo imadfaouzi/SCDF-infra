e21032f7d1ff0a2ef2586ee55cd6233b314d126d
imadfaouzi <faouziimad2001@gmail.com>
2024-05-22 12:24:38 +0100

    saving tfstate testing stage-0

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 
 
-module "my_vpc" {
-  source          = "./modules/vpc_module"
-  vpc_cidr        = "10.0.0.0/16"
-  vpc_name        = "my-vpc"    #not required
-  azs             = ["us-east-1a", "us-east-1b"]
+# module "my_vpc" {
+#   source          = "./modules/vpc_module"
+#   vpc_cidr        = "10.0.0.0/16"
+#   vpc_name        = "my-vpc"    #not required
+#   azs             = ["us-east-1a", "us-east-1b"]
 
-  public_subnets  = ["10.0.2.0/24", "10.0.4.0/24"]
-  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.2.0/24", "10.0.4.0/24"]
+#   private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
 
 
-module "dataflowRepo" {
+module "testRepo1" {
   source = "./modules/ECR"
 
   account_id          = var.AccountID
   region              = var.aws_region
-  repository_name     = "dataflow"
+  repository_name     = "testRepo1"
   tag                 = "latest"
 
   image_tag_mutability = "IMMUTABLE"
@@ -32,23 +32,20 @@ module "dataflowRepo" {
   }
 }
 
-module "SkipperRepo" {
-  source = "./modules/ECR"
+# module "SkipperRepo" {
+#   source = "./modules/ECR"
 
-  account_id          = var.AccountID
-  region              = var.aws_region
-  repository_name     = "skipper"
-  tag                 = "latest"
+#   account_id          = var.AccountID
+#   region              = var.aws_region
+#   repository_name     = "skipper"
+#   tag                 = "latest"
 
-  image_tag_mutability = "IMMUTABLE"
-  scan_on_push         = true
-  is_public            = false
+#   image_tag_mutability = "IMMUTABLE"
+#   scan_on_push         = true
+#   is_public            = false
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
 
-output "dataflowURL" {
-  value = module.dataflowRepo.repository_url
-}
